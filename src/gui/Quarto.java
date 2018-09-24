4a3a323c1bc20ca2bd83bc4e794e489240b6a014
PaddyKe <34421580+paddyke@users.noreply.github.com>
2018-09-24 20:45:58 +0200

    Renamed from Main to Quarto

--- src/gui/Quarto.java
+++ src/gui/Quarto.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Quarto extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
