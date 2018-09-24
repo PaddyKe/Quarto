1d9cd19e1d39f1bfc5299f341e68d63682cf360c
PaddyKe <34421580+paddyke@users.noreply.github.com>
2018-09-24 17:19:04 +0200

    Added RandomPlayer... Player completely random.

---
+++ src/player/RandomPlayer.java
@@ -0,0 +1,30 @@
+package player;
+
+import game.Figure;
+import interfaces.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomPlayer extends Player {
+
+    public RandomPlayer() {
+        super("Rainer Zufall");
+    }
+
+    @Override
+    public int placeFigure(Figure f, Figure[] board, List<Figure> remaining) {
+        List<Integer> field = new ArrayList<Integer>();
+        for (int i = 0; i < board.length; i++) {
+            if(board[i] == null) {
+                field.add(i);
+            }
+        }
+        return field.get(this.rnd.nextInt(field.size()));
+    }
+
+    @Override
+    public Figure selectFigure(List<Figure> remaining, Figure[] board) {
+        return remaining.get(this.rnd.nextInt(remaining.size()));
+    }
+}
